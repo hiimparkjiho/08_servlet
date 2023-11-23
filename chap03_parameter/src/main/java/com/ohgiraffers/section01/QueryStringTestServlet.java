2877c082643491caaeab459dec47c901e5a75cd3
hiimparkjiho <jiho2689@gmail.com>
2023-11-23 18:28:01 +0900

    servlet

--- chap03_parameter/src/main/java/com/ohgiraffers/section01/QueryStringTestServlet.java
+++ chap03_parameter/src/main/java/com/ohgiraffers/section01/QueryStringTestServlet.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.section01;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
