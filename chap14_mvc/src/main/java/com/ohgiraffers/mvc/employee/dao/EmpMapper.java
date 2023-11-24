dee596f10c0b967fa021be911f70ec357cfdfec7
hiimparkjiho <jiho2689@gmail.com>
2023-11-24 18:20:04 +0900

    servlet

---
+++ chap14_mvc/src/main/java/com/ohgiraffers/mvc/employee/dao/EmpMapper.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.mvc.employee.dao;
+
+import com.ohgiraffers.mvc.employee.dto.EmpDTO;
+import com.ohgiraffers.mvc.employee.dto.EmpInsertDTO;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface EmpMapper {
+//    @Results( id = "emp", value = {
+//            @Result(id = true, property = "emp_id", column = "EMP_ID"),
+//            @Result(property="empName", column="EMP_NAME"),
+//            @Result(property="email", column="EMAIL"),
+//            @Result(property="phone", column="PHONE")
+//    })
+//    @Select(
+//           "        SELECT\n" +
+//                   "            EMP_ID,\n" +
+//                   "            EMP_NAME,\n" +
+//                   "            EMAIL,\n" +
+//                   "            PHONE\n" +
+//                   "        FROM EMPLOYEE\n" +
+//                   "        WHERE EMP_ID = #{empId}"
+//    )
+
+    EmpDTO selectByEmpId(int empId);
+
+    List<EmpDTO> selectAllEmp();
+
+    int insert(EmpInsertDTO insert);
+}
